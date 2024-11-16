1a720122a9ad74aaf8f7ec3cd42ce31e60017ea8
hamidthedev1 <hamidabuhayat1@gmail.com>
2024-11-16 04:15:47 +0300

    Row and RowList

--- netflix-clone/src/pages/Home/Home.jsx
+++ netflix-clone/src/pages/Home/Home.jsx
@@ -2,12 +2,14 @@
 import Header from '../../components/Header/Header'
 import Banner from '../../components/Banner/Banner'
 import Footer from '../../components/Footer/Footer'
+import RowList from '../../components/Rows/RowList/RowList'
 
 function Home() {
   return (
     <>
       <Header />
       <Banner />
+      <RowList />
       <Footer />
     </>
   )
